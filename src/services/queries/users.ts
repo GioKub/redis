08602db9e7349a65fde05c5f03a90b624ddb3dfd
GioKub <gkublashvili@unisens.ge>
2022-05-31 16:14:33 +0400

    added function for adding user to the redis database

--- src/services/queries/users.ts
+++ src/services/queries/users.ts
@@ -1,5 +1,18 @@
+import { client } from "$services/redis";
+import { usersKey } from "$services/keys";
+import type { CreateUserDto } from "$services/types";
+import { genId } from "$services/utils";
+
 export const getUserByUsername = async (username: string) => {};
 
 export const getUserById = async (id: string) => {};
 
-export const createUser = async () => {};
+export const createUser = async (attrs: CreateUserDto) => {
+    const id = genId()
+    await client.HSET(usersKey(id),{
+        username: attrs.username,
+        password: attrs.password
+    })
+
+    return id
+};
