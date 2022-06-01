39c10baaa47fb40b4a1602bcd652b34ec421a412
GioKub <gkublashvili@unisens.ge>
2022-06-01 17:07:54 +0400

    implement likeITem and unlikeItem functions

--- src/services/queries/likes.ts
+++ src/services/queries/likes.ts
@@ -1,9 +1,16 @@
+import { client } from "$services/redis";
+import { userLikesKey } from "$services/keys";
+
 export const userLikesItem = async (itemId: string, userId: string) => {};
 
 export const likedItems = async (userId: string) => {};
 
-export const likeItem = async (itemId: string, userId: string) => {};
+export const likeItem = async (itemId: string, userId: string) => {
+    await client.SADD(userLikesKey(userId), itemId)
+};
 
-export const unlikeItem = async (itemId: string, userId: string) => {};
+export const unlikeItem = async (itemId: string, userId: string) => {
+    await client.SREM(userLikesKey(userId), itemId)
+};
 
 export const commonLikedItems = async (userOneId: string, userTwoId: string) => {};
