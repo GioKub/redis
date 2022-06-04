32bd84cde467fb97e35bff42b09f73d1585015ee
GioKub <gkublashvili@unisens.ge>
2022-06-04 19:42:03 +0400

    now when item is created, it will also be saved into "items:endingAt" sorterd set with it"s id as member and default of 1 as score

--- src/services/queries/views.ts
+++ src/services/queries/views.ts
@@ -1,1 +1,9 @@
-export const incrementView = async (itemId: string, userId: string) => {};
+import { client } from "$services/redis";
+import { itemsByViewsKey, itemsKey } from "$services/keys";
+
+export const incrementView = async (itemId: string, userId: string) => {
+    return Promise.all([
+        client.HINCRBY(itemsKey(itemId), 'views', 1),
+        client.ZINCRBY(itemsByViewsKey(), 1, itemId)
+    ])
+};
