ac14c821625225882167eb2434f3d55e64c39eda
GioKub <gkublashvili@unisens.ge>
2022-06-06 14:04:39 +0400

    now same user can"t add multiple views to item

--- src/services/queries/views.ts
+++ src/services/queries/views.ts
@@ -1,9 +1,13 @@
-import { client } from "$services/redis";
-import { itemsByViewsKey, itemsKey } from "$services/keys";
+import { client } from '$services/redis';
+import { itemsByViewsKey, itemsKey, itemsViewsKey } from '$services/keys';
 
 export const incrementView = async (itemId: string, userId: string) => {
-    return Promise.all([
-        client.HINCRBY(itemsKey(itemId), 'views', 1),
-        client.ZINCRBY(itemsByViewsKey(), 1, itemId)
-    ])
+	const inserted = await client.PFADD(itemsViewsKey(itemId), userId);
+
+	if (inserted) {
+		return Promise.all([
+			client.HINCRBY(itemsKey(itemId), 'views', 1),
+			client.ZINCRBY(itemsByViewsKey(), 1, itemId)
+		]);
+	}
 };
