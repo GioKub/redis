1d47773aff0b52517c66bb838b2bb80aba065e14
GioKub <gkublashvili@unisens.ge>
2022-06-13 15:31:33 +0400

    added functino for creating indexes

--- src/services/redis/create-indexes.ts
+++ src/services/redis/create-indexes.ts
@@ -1,1 +1,21 @@
-export const createIndexes = async () => {};
+import { SchemaFieldTypes } from "redis";
+import { itemsIndexKey, itemsKey } from "$services/keys";
+import {client} from './client'
+
+export const createIndexes = async () => {
+    return client.ft.create(
+        itemsIndexKey(),
+        {
+            name: {
+                type: SchemaFieldTypes.TEXT
+            },
+            description:{
+                type: SchemaFieldTypes.TEXT
+            }
+        },
+        {
+            ON: 'HASH',
+            PREFIX: itemsKey('')
+        }
+    )
+};
